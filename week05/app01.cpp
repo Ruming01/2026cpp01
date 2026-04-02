588b6a82d1817628784ba0143ed443055fabb71d
Ruming01 <youhyun0609@naver.com>
2026-04-02 12:18:55 +0900

    week05 v0.2 heap memory control

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -4,5 +4,32 @@ using namespace std;
 
 int main() {
 
+	//int humans = 100;
+	//int ages[humans];
+	
+	int age;
+	//int* a = &age;
+
+	int humans = 0;
+	cout << "몇 명? ";
+	cin >> humans;
+	int* ages = new int[humans]; // heap memory 
+
+	for (int i = 0; i < humans; i++) {
+		cout << "나이? ";
+		cin >> age;
+		*(ages + i) = age;
+	}
+
+	for (int i = 0; i < humans; i++) {
+		cout << *(ages + i) << "\n";
+	}
+
+	//cout << ages << '\n';
+	//cout << &humans << '\n';
+	//cout << &age << '\n';
+
+	delete[] ages; //free memory
+
 	return 0;
 }
