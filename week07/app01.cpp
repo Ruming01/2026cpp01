a32a02388e38f6c5b7523af1b01cd3739a2188cf
Ruming01 <youhyun0609@naver.com>
2026-04-16 12:15:24 +0900

    week07 memory management v0.3

--- week07/app01.cpp
+++ week07/app01.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 using namespace std;
 
-//2d array : statck memory & heap memory
+//2d array : only heap memory
 
 int main() {
+	int rows = 0, columns = 0;
+	int** arr = nullptr; 
 
-	int columns = 0;
-	int* arr[3]; //stack memory
+	cin >> rows;
 	cin >> columns;
 
+	arr = new int* [rows]; // heap memory에 포인터 배열을 동적 할당
 
-
-	for (int i = 0; i < 3; i++) {
-		arr[i] = new int[columns]; //heap memory
+	for (int i = 0; i < rows; i++) {
+		arr[i] = new int[columns]; // heap memory에 정수 타입을 담는 배열 동적 할당
 	}
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			arr[i][j] = i * j;
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < rows; i++) {
 		for (int j = 0; j < columns; j++) {
 			cout << arr[i][j] << " ";
 		}
 		cout << '\n';
 	}
 
-	for (int i = 0; i < 3; i++) {
-		delete[] arr[i];
-		arr[i] = nullptr;
+	for (int i = 0; i < rows; i++) {
+		delete[] arr[i]; // 각 행에 해당하는 배열을 해제
+		arr[i] = nullptr; // 포인터를 nullptr로 초기화하여 dangling pointer 방지
 	}
+
+	delete[] arr;
+	arr = nullptr; // 포인터를 nullptr로 초기화하여 dangling pointer 방지
 
 	return 0;
 }
