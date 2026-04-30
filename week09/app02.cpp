0910b636773888890b3c626876195ae2d388b4c8
Ruming01 <youhyun0609@naver.com>
2026-04-30 12:33:27 +0900

    week09 : string v2.0

--- week09/app02.cpp
+++ week09/app02.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-    char lines[80];
-    cout << "한 줄을 입력하세요: ";
-    cin.getline(lines, 80); // (엔터키만 구분자로 인식)
-	// cin >> lines; // (스페이스바, 탭, 엔터키는 모두 구분자로 인식)
-    cout << endl;
-    cout << "출력: " << endl;
-    cout << lines << endl;
+    char a[] = "ACE";
+	char b[] = "ACE";
+	string c = "ACE";
+	string d = "ACE";
+
+	cout << strcmp(a, b) << '\n'; // -1, 0, 1 return
+	cout << (c == d) << '\n'; // true, false return
+
     return 0;
 }
