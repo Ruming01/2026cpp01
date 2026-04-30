c6fe42d333ba8cc236818b47a0d252d7f83e68b9
Ruming01 <youhyun0609@naver.com>
2026-04-30 11:18:54 +0900

    week09 : string v1.5

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -9,8 +9,13 @@ int main()
 	//char s1[] = { 'A', 'C', 'E'}; // null문자 부재로 오류 발생
 	//char s1[] = { 'A', 'C', 'E', NULL };
 	//char s1[] = { 'A', 'C', 'E', 0 };
-	char s1[] = "ACE"; // null문자 자동 추가
+	//char s1[] = "ACE"; // null문자 자동 추가
+	
+	char s1[] = { 'A', 'C', NULL, 'E'}; 
 	cout << s1 << '\n'; 
+	
+	s1[2] = 'D';
+	cout << s1 << '\n';
 
 	return 0;
 }
