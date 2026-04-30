416ffe51c6c8ca79a6c99a628f3c55db31f83405
Ruming01 <youhyun0609@naver.com>
2026-04-30 11:40:22 +0900

    week09 : string v1.7

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -5,22 +5,11 @@ using namespace std;
 
 int main()
 {
-	//char s1[] = { 'A', 'C', 'E', '\0' };
-	//char s1[] = { 'A', 'C', 'E'}; // null문자 부재로 오류 발생
-	//char s1[] = { 'A', 'C', 'E', NULL };
-	//char s1[] = { 'A', 'C', 'E', 0 };
-	char s1[] = "ACE"; // null문자 자동 추가
-	char* ps1 = s1; // = char* ps1 = &s1[0];
-	
+
+	char s1[] = "ACE"; // array compact initializer
+	const char* ps1 = "ACE"; // string ilteral
 	cout << s1 << '\n';
 	cout << ps1 << '\n';
-	cout << *ps1 << '\n';
-	cout << *s1 << '\n';
-	cout << s1[1] << '\n';
-	cout << *(ps1+1) << '\n';
-
-	*(ps1 + 2) = 'Z'; // = ps1[2] = 'Z';
-	cout << s1 << '\n';
 
 	return 0;
 }
