a7cecf3e83dc8d23a6ee08a6581a44c9ad92318c
Ruming01 <youhyun0609@naver.com>
2026-04-30 11:57:56 +0900

    week09 : string v1.8

--- week09/app01.cpp
+++ week09/app01.cpp
@@ -2,6 +2,17 @@
 #include <cstring> // c언어 string.h
 using namespace std;
 
+int myStrlen(const char* s) {
+	
+	int count = 0;
+
+	while (s[count] != '\0') { // while (s[i] != 0) or while(s[i] != NULL) or while(*s != '\0')
+		count++;
+	}
+
+	return count;
+}
+
 
 int main()
 {
@@ -10,6 +21,12 @@ int main()
 	const char* ps1 = "ACE"; // string ilteral
 	cout << s1 << '\n';
 	cout << ps1 << '\n';
+	cout << strlen(s1) << '\n';
+	cout << strlen(ps1) << '\n';
+	cout << strlen(&s1[0]) << '\n';
+	cout << myStrlen(s1) << '\n';
+
+
 
 	return 0;
 }
