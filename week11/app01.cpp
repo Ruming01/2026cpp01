31ab7b5d3675d21198d8d8dfde6f339fa23926f1
Ruming01 <youhyun0609@naver.com>
2026-05-14 11:44:26 +0900

    week11 : str v3.0

--- week11/app01.cpp
+++ week11/app01.cpp
@@ -3,21 +3,27 @@
 #include <iomanip>
 using namespace std;
 
-int main() {
+void reverse(string& s)
+{
+	string temp = s;
+	int size = s.size();
+	for (int i = 0; i < size; i++)
+	{
+		s[i] = temp[size - 1 - i];
+	}
+}
+
+int main()
+{
 	string s1("abc");
 	string s2 = "abcd";
-	cout << (s1 == s2) << endl;
-	cout << boolalpha << (s1 == s2) << endl; // true false 형태 지정
-	cout << (s1 == s2) << endl; // 한번 지정되고 나면 형태 고정
-	cout << noboolalpha; // 0 , 1 형태로 재변경
-	cout << (s1 == s2) << endl;
 
-	cin >> s2;
-	cout << s2 << endl;
-	//getline(cin, s2, ' ');
-	// 남은 더미 값을 제거하기 위해 버퍼 비우기
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 	getline(cin, s2);
+	//cin >> s2;
+	cout << s2 << endl;
+	cout << s2[1] << endl;
+	cout << s2.at(1) << endl;
+	reverse(s2);
 	cout << s2 << endl;
 
 	return 0;
