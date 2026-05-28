03b0d09578d02e94e431fbb49af8cc8c03cd7baf
Ruming01 <youhyun0609@naver.com>
2026-05-28 11:20:29 +0900

    week13 : inheritance v3.6

--- week13/app01.cpp
+++ week13/app01.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <string>
-using namespace std;
+
+#include "pokemon.h"
+#include "pikachu.h"
 
 
 
--- week13/pokemon.h
+++ week13/pokemon.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Pokemon
+{
+private:
+    int hp;
+public:
+    Pokemon();
+    Pokemon(int hp);
+    Pokemon(const Pokemon& pokemon);
+    void set(int hp);
+    int getHp() const;
+};
